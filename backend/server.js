0e28850c0e550c90215d329ffd6865b4ba67bc9c
Al Noman Mahib <alnomanmatubber2001@gmail.com>
2023-02-08 21:45:17 +0600

    half done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 const app = express();
 dotenv.config();
+app.use(cors());
+app.use(express.json());
 const port = process.env.PORT;
 
 const notes = require("./data/notes");
